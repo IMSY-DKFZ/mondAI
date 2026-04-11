10fe26e5af728bdbae1261193a8834755072cd2c
Marco Hübner <marco.huebner@dkfz-heidelberg.de>
2026-04-11 12:12:43 +0200

    Add MSE to convenience metrics init

--- src/mondAI/metrics/__init__.py
+++ src/mondAI/metrics/__init__.py
@@ -1,4 +1,5 @@
 from mondAI.metrics.full_reference.haarpsi import HaarPSI as HaarPSI
 from mondAI.metrics.full_reference.mae import MAE as MAE
+from mondAI.metrics.full_reference.mse import MSE as MSE
 from mondAI.metrics.full_reference.msssim import MSSSIM as MSSSIM
 from mondAI.metrics.full_reference.ssim import SSIM as SSIM
