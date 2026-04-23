9432bf0e178491cbbec48134e7e0a86745928e91
Tom Rix <tom.rix@dkfz-heidelberg.de>
2026-04-23 16:32:24 +0200

    Top level metrics import

--- src/mondAI/metrics/__init__.py
+++ src/mondAI/metrics/__init__.py
@@ -1,8 +1,12 @@
 from mondAI.metrics.full_reference.cwssim import CWSSIM as CWSSIM
+from mondAI.metrics.full_reference.fsim import FSIM as FSIM
 from mondAI.metrics.full_reference.haarpsi import HaarPSI as HaarPSI
+from mondAI.metrics.full_reference.haarpsi import HaarPSI_MED as HaarPSI_MED
 from mondAI.metrics.full_reference.iwssim import IWSSIM as IWSSIM
 from mondAI.metrics.full_reference.mae import MAE as MAE
 from mondAI.metrics.full_reference.mse import MSE as MSE
 from mondAI.metrics.full_reference.msssim import MSSSIM as MSSSIM
 from mondAI.metrics.full_reference.psnr import PSNR as PSNR
 from mondAI.metrics.full_reference.ssim import SSIM as SSIM
+from mondAI.metrics.full_reference.vifp import VIFP as VIFP
+from mondAI.metrics.no_reference.paq2piq import PaQ2PiQ as PaQ2PiQ
