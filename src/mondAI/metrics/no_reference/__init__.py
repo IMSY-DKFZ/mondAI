e1cde46920d0c9655fa2af2e0a72bae00b700ec4
Tom Rix <tom.rix@dkfz-heidelberg.de>
2026-06-01 18:11:08 +0200

    Removed NIQE from tests and metric list, as it's computation is instable, but still keep it in package for comparison reasons

--- src/mondAI/metrics/no_reference/__init__.py
+++ src/mondAI/metrics/no_reference/__init__.py
@@ -1,4 +1,3 @@
-from mondAI.metrics.no_reference.niqe import NIQE
 from mondAI.metrics.no_reference.paq2piq import PaQ2PiQ
 
-NO_REFERENCE_METRICS = [PaQ2PiQ, NIQE]
+NO_REFERENCE_METRICS = [PaQ2PiQ]
