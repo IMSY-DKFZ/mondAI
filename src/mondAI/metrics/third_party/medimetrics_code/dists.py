72bae0741022af9f8ffe47e8b2a2db3d54230f9e
Tom Rix <tom.rix@dkfz-heidelberg.de>
2026-06-12 18:34:21 +0200

    Fixed third party dists weight path

--- src/mondAI/metrics/third_party/medimetrics_code/dists.py
+++ src/mondAI/metrics/third_party/medimetrics_code/dists.py
@@ -72,7 +72,7 @@ class DISTNet(torch.nn.Module):
         self.alpha.data.normal_(0.1, 0.01)
         self.beta.data.normal_(0.1, 0.01)
 
-        BASE = Path(__file__).resolve().parent.parent
+        BASE = Path(__file__).resolve().parent.parent.parent
         weights = torch.load(BASE / "weights" / "dists.pt")
         self.alpha.data = weights["alpha"]
         self.beta.data = weights["beta"]
