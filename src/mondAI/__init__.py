925c5d26ab4faa5f0442502eb9f98bac76e8c4b5
Tom Rix <tom.rix@dkfz-heidelberg.de>
2026-06-16 14:05:38 +0200

    Added citation reminder

--- src/mondAI/__init__.py
+++ src/mondAI/__init__.py
@@ -1,3 +1,22 @@
 from importlib.metadata import version
 
+from .logging import get_logger
+
 __version__ = version("mondAI")
+
+
+logger = get_logger()
+logger.info(
+    "\n"
+    "**********************************************************\n"
+    "                   CITATION REMINDER                      \n"
+    "**********************************************************\n"
+    "If you use any of the metrics implemented in the mondAI   \n"
+    "library for your research, please consider citing this    \n"
+    "repository and leaving a 🌟 in addition to citing the     \n"
+    "original papers that introduced those metrics.            \n"
+    "Proper citation helps acknowledge the work of the         \n"
+    "researchers who developed these metrics and supports      \n"
+    "the continued advancement of the field.\n"
+    "**********************************************************\n"
+)
